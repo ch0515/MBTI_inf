0d5b4b8a0a155b9b9009772fb930e164e7127eaa
ch0515 <w2114@e-mirim.hs.kr>
2022-11-17 21:31:55 +0900

    result 화면 결과 계산하기

--- MyMBTI-main/start/js/start.js
+++ MyMBTI-main/start/js/start.js
@@ -2,7 +2,46 @@ const main = document.querySelector("#main"); //id 값 main
 const qna = document.querySelector("#qna");
 const result = document.querySelector("#result");
 const endPoint = 12;
+const select = [];
 
+function calResult(){
+  var pointArray = [
+    { name: 'mouse', value: 0, key:0 },
+    { name: 'cow', value: 0, key:1 },
+    { name: 'tiger', value: 0, key:2 },
+    { name: 'rabbit', value: 0, key:3 },
+    { name: 'dragon', value: 0, key:4 },
+    { name: 'snake', value: 0, key:5 },
+    { name: 'horse', value: 0, key:6 },
+    { name: 'sheep', value: 0, key:7 },
+    { name: 'monkey', value: 0, key:8 },
+    { name: 'chick', value: 0, key:9 },
+    { name: 'dog', value: 0, key:10 },
+    { name: 'pig', value: 0, key:11 },
+  ]
+  for(let i = 0; i < endPoint; i++){
+    var target = qnaList[i].a[select[i]];
+    for(let j = 0; j < target.type.length; j++){
+      for(let k = 0; k < pointArray.length; k++){
+        if(target.type[j] === pointArray[k].name){
+          pointArray[k].value += 1;
+        }
+      }
+    }
+  }
+  var resultArray = pointArray.sort(function (a, b){
+    if(a.value > b.value){
+      return -1;
+    }
+    if(a.value < b.value){
+      return 1;
+    }
+    return 0;
+  });
+
+  let resultword = resultArray[0].key;
+  return resultword;
+}
 function goResult(){
   qna.style.WebkitAnimation = "fadeOut 1s";
   qna.style.animation = "fadeOut 1s";
@@ -13,9 +52,10 @@ function goResult(){
         qna.style.display = "none";
         result.style.display = "block";
     }, 450)});
+
 }
 
-function addAnswer(answerText, qIdx){
+function addAnswer(answerText, qIdx, idx){
   var a = document.querySelector('.answerBox');
   var answer = document.createElement('button');
   answer.classList.add('answerList');
@@ -34,6 +74,7 @@ function addAnswer(answerText, qIdx){
       children[i].style.animation = "fadeOut 0.5s";
     }
     setTimeout(() => {
+      select[qIdx] = idx;
       for(let i = 0; i < children.length; i++){
         children[i].style.display = 'none';
       }
@@ -42,14 +83,14 @@ function addAnswer(answerText, qIdx){
   }, false);
 }
 function goNext(qIdx){
-  if(qIdx+1 === endPoint){
+  if(qIdx === endPoint){
     goResult();
     return;
   }
   var q = document.querySelector('.qBox');
   q.innerHTML = qnaList[qIdx].q;
   for(let i in qnaList[qIdx].a){
-    addAnswer(qnaList[qIdx].a[i].answer, qIdx);
+    addAnswer(qnaList[qIdx].a[i].answer, qIdx, i);
   }
   var status = document.querySelector('.statusBar');
   status.style.width = (100/endPoint) * (qIdx+1) + '%';
